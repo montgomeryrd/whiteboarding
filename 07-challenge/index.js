ef92fb6230805e542091e6dd9e225d95ddc51d14
montgomeryrd <montgomery_rd@yahoo.com>
2018-01-24 21:54:59 -0800

    all tests passing now

--- 07-challenge/index.js
+++ 07-challenge/index.js
@@ -6,14 +6,19 @@ typeOf.list = function(data) {
     if(!data || typeof data !== 'object'){ //If there is no data or the data is not an object
         return null; //return null
     }
-    let copy = Object.assign(data); //make a copy of the data to a new variable called copy 
-    while (copy) { //while the copy list is... the copy list
-        if(copy.seen){ //check if copy has a property of seen
-            return true; //return true if so
-        } else { //or else
-            copy.seen = true; //add a property called seen with the value true
-            copy = copy.next || copy.head; //iterate to next node
+ 
+    let copy = Object.assign({}, data); //make a copy of the data to a new variable called copy 
+ 
+    while(copy.head) { //while the copy list is... the copy list
+        copy = copy.head;
+
+        while(copy.next) {
+            copy = copy.next;
+        
+            if(copy.next === copy.head){ //check if copy has a property of seen
+                return true; //return true if so
+            }
+            return false; //return false if there's no more copy
         }
     }
-    return false; //return false if there's no more copy
 };
