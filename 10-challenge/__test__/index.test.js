64bffba36fa9a2c5a0c1a1d259bcf8addf02032e
montgomeryrd <montgomery_rd@yahoo.com>
2018-01-28 15:26:29 -0800

    Finished second whiteboarding problem

--- 10-challenge/__test__/index.test.js
+++ 10-challenge/__test__/index.test.js
@@ -5,60 +5,95 @@ const Stack = require('../lib/stack');
 const challenge = require('../lib/index');
 require('jest');
 
-// describe('challenge module', () => {
+describe('challenge module', () => {
 
-//     let shittyString = new Node ('} No longer {} stranded }like a painted { bird } on a{ fan}');
-//     let validString = new Node('{There is something in the way the light comes shining through.}');
+    let shittyString = new Node ('} No longer {} stranded }like a painted { bird } on a{ fan}');
+    let validString = new Node('{There is something in the way the light comes shining through.}');
     
-//     describe('#Test Node', () => {
-//         it('should test if new node is an object.', () => {
-//             expect(typeof shittyString).toBe('object');
-//             expect(typeof validString).toBe('object');
-//         });
-//     });
+    describe('#Test Node', () => {
+        it('should test if new node is an object.', () => {
+            expect(typeof shittyString).toBe('object');
+            expect(typeof validString).toBe('object');
+        });
+    });
 
-//     describe('#checkBrace Valid', () => {
+    describe('#checkBrace Valid', () => {
     
-//         let stack = new Stack();
-//         stack.push(validString);
+        let stack = new Stack();
+        stack.push(validString);
     
-//         it('should check if the input is a valid string', () => {
-//             expect(typeof validString.val).toBe('string');
-//         });
-//         it('should return that something is in the stack', () => {
-//             expect(stack.size).toEqual(1);
-//         });
-//         it('should return that this stack is awesome!', () => {
-//             let stack = new Stack();
-//             stack.push(validString);
-//             expect(challenge.checkBrace(stack)).toEqual('This stack is awesome!');
-//         });
-//     });
+        it('should check if the input is a valid string', () => {
+            expect(typeof validString.val).toBe('string');
+        });
+        it('should return that something is in the stack', () => {
+            expect(stack.size).toEqual(1);
+        });
+        it('should return that this stack is awesome!', () => {
+            let stack = new Stack();
+            stack.push(validString);
+            expect(challenge.checkBrace(stack)).toEqual('This stack is awesome!');
+        });
+    });
 
-//     describe('#checkBrace Invalid', () => {
+    describe('#checkBrace Invalid', () => {
 
-//         let stack = new Stack();
+        let stack = new Stack();
 
-//         it('should check for any input and return that the stack is empty', () => {
-//             expect(challenge.checkBrace(stack)).toBe('This stack is empty...');
-//         });
+        it('should check for any input and return that the stack is empty', () => {
+            expect(challenge.checkBrace(stack)).toBe('This stack is empty...');
+        });
         
-//         it('should return null if nothing is passed in.', () => {
-//             expect(challenge.checkBrace()).toBeNull();
-//         });
+        it('should return null if nothing is passed in.', () => {
+            expect(challenge.checkBrace()).toBeNull();
+        });
 
-//         let newstack = new Stack();
-//         newstack.push(validString);
+        let newstack = new Stack();
+        newstack.push(validString);
 
-//         it('should return This stack is awesome!', () => {
-//             expect(challenge.checkBrace(newstack)).toBe('This stack is awesome!');
-//         });
+        it('should return This stack is awesome!', () => {
+            expect(challenge.checkBrace(newstack)).toBe('This stack is awesome!');
+        });
 
-//         let newerstack = new Stack();
-//         newerstack.push(shittyString);
+        let newerstack = new Stack();
+        newerstack.push(shittyString);
         
-//         it('should return This stack sucks.', () => {
-//             expect(challenge.checkBrace(newerstack)).toBe('This stack sucks.');
-//         });
-//     });
-// });
+        it('should return This stack sucks.', () => {
+            expect(challenge.checkBrace(newerstack)).toBe('This stack sucks.');
+        });
+    });    
+});
+
+describe('challenge module', () => {
+
+    let invalidArray = [2, 'thrice', 'the', 'window', 10];
+    let validAF = [9, 1, 8, 4, 3, 7];
+    
+    describe('#binarySearch Valid', () => {
+        
+        it('should check if the input is valid', () => {
+            let n = 7;
+            expect(typeof n).toBe('number');
+            expect(Array.isArray(validAF)).toBe(true);
+        });
+        it('should sort the array just in case it isn\' already sorted', () => {
+            expect(validAF.sort(function(a, b) {
+                return a - b;
+            })).toEqual([1, 3, 4, 7, 8, 9]);
+        });
+        it('should return expected results', () => {
+            expect(challenge.binarySearch(7, validAF)).toEqual({value: 7, index: 3});
+        });
+    });
+
+    describe('#binarySearch Invalid', () => {
+
+        it('should check if the input is invalid', () => {
+            expect(challenge.binarySearch('soda', validAF)).toBe('Not valid input');
+            expect(challenge.binarySearch(16, 'Tacoma')).toBe('Not valid input');
+            expect(challenge.binarySearch(7, invalidArray)).toBe('Not valid input');
+        });
+        it('should return null if index is not in array.', () => {
+            expect(challenge.binarySearch(5, validAF)).toBeNull();
+        });
+    });
+});
--- 10-challenge/lib/index.js
+++ 10-challenge/lib/index.js
@@ -3,26 +3,29 @@
 // const Stack = require('./stack');
 const challenge = module.exports = {};
 
+//This function takes a stack as its input and pops off each element to check its value for pairs of ordered curly braces => { }.
 challenge.checkBrace = function(stack) {
 
-    // let stack = new Stack();
     if(!stack) return null;
 
     if(stack.top === null) return 'This stack is empty...';
 
-    let top = stack.pop();
-    let object = top.val;
-    object = `'${object.val}'`;
-    object = object.split('');
+    for(let h = 0 ; stack.size > 0 ; h++) {
 
-    for(let i = 0 ; i < object.length ; i++) {
-        if(object[i] === '{') stack.push(object[i]);
-        if(object[i] ==='}') {
-            if(stack.size === 0) return 'This stack sucks.';
-            stack.pop();
+        let array = [];
+        let top = stack.pop();
+        let object = top.val;
+        object = `'${object.val}'`;
+        object = object.split('');
+
+        for(let i = 0 ; i < object.length ; i++) {
+            if(object[i] === '{') array.push(object[i]);
+            if(object[i] === '}') array.push(object[i]);
         }
+
+        if(array[0] === '}' || array[array.length - 1] === '{') return 'This stack sucks.';
     }
-    
+
     if(stack.size === 0) return 'This stack is awesome!';
     
 };
@@ -64,9 +67,29 @@ challenge.checkBrace = function(stack) {
 // };
 
 
-
+//This function takes in two arguments, a number and an array of numbers, and does a binary search to locate the number inside the array. 
 challenge.binarySearch = function(n, array) {
+    if(!n || !array) return 'Not valid input';
+    if(typeof n !== 'number' || !Array.isArray(array)) return 'Not valid input';
 
+    for(let i = 0 ; i < array.length ; i++){
+        if(typeof array[i] !== 'number') return 'Not valid input';
+    }
+    //I don't know how to check if it's sorted so... sort it just in case.
+    array.sort(function(a, b) {
+        return a - b;
+    });
+
+    let min = 0;
+    let max = array.length - 1;
+    let here;
+
+    while(min <= max) {
+        here = Math.floor((max + min)/2);
+        if(array[here] === n) return {value: n, index: here};
+        if(array[here] < n) min = here + 1; 
+        if(array[here] > n) max = here - 1;
+    }
+
+    return null;
 };
-
-
