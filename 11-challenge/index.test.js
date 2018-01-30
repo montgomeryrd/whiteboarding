c796f481ccfddad02c172f1d71dd1b10dd6e0efd
montgomeryrd <montgomery_rd@yahoo.com>
2018-01-29 19:59:33 -0800

    committing name change

--- 11-challenge/index.test.js
+++ 11-challenge/index.test.js
@@ -1,22 +1,22 @@
 'use strict';
 
-const find = require('./index');
+const calculateMissing = require('./index');
 require('jest');
 
 
-describe('find', () => {
+describe('calculateMissing', () => {
     describe('#valid', () => {
         it('should check if input is invalid', () => {
-            expect(find()).toEqual('Invalid input');
-            expect(find('lumpy', 'po', 'tus')).toEqual('Invalid input');
+            expect(calculateMissing()).toEqual('Invalid input');
+            expect(calculateMissing('lumpy', 'po', 'tus')).toEqual('Invalid input');
         });
         it('should check for valid input', () => {
             expect(Array.isArray([1, 2, 3, 5])).toBe(true);
         });
         it('should return the correct answer', () => {
-            expect(find([1, 2, 3, 5])).toEqual(4);
-            expect(find([1, 2, 3, 4, 5, 6, 8, 9, 10])).toEqual(7);
-            expect(find([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 18, 19])).toEqual(17);
+            expect(calculateMissing([1, 2, 3, 5])).toEqual(4);
+            expect(calculateMissing([1, 2, 3, 4, 5, 6, 8, 9, 10])).toEqual(7);
+            expect(calculateMissing([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 18, 19])).toEqual(17);
         });
 
     })
