f20db5ccf9a7c9553c0355c63c13ed9675a74b79
montgomeryrd <montgomery_rd@yahoo.com>
2018-02-26 22:17:11 -0800

    Misread the challenge the first time

--- 31-challenge/solution.js
+++ 31-challenge/solution.js
@@ -2,16 +2,15 @@
 
 const wb = module.exports = {};
 
-wb.sect = function(array){
+wb.sect = function(arr1, arr2){
+    if(!(Array.isArray(arr1)) || !(Array.isArray(arr2))) return new Error('Invalid Input');
 
-    if(!(Array.isArray(array))) return new Error('Invalid Input');
- 
-    let i = (array.length/2);
-
-    if(Number.isInteger(i)){
-        return [array[i - 1], array[i]];
-    } else {
-        i = Math.floor(i);
-        return [array[i]];
+    for(let a = 0 ; a < arr1.length ; a++){
+        for(let b = 0 ; b < arr2.length ; b++){
+            if(arr1[a] === arr2[b] && arr1[a + 1] === arr2[b + 1]){
+                return [arr1[a], arr1[a + 1]];
+            }
+        }
     }
+    return 'No intersection';
 };
