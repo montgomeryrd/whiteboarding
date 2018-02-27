f3382688051db8f8ce6114ef0a249382fafb86ba
montgomeryrd <montgomery_rd@yahoo.com>
2018-02-27 01:04:52 -0800

    misunderstood again

--- 31-challenge/solution.js
+++ 31-challenge/solution.js
@@ -2,15 +2,19 @@
 
 const wb = module.exports = {};
 
-wb.sect = function(arr1, arr2){
+wb.intersection = function(arr1, arr2){
     if(!(Array.isArray(arr1)) || !(Array.isArray(arr2))) return new Error('Invalid Input');
+
+    let answer = [];
 
     for(let a = 0 ; a < arr1.length ; a++){
         for(let b = 0 ; b < arr2.length ; b++){
-            if(arr1[a] === arr2[b] && arr1[a + 1] === arr2[b + 1]){
-                return [arr1[a], arr1[a + 1]];
+            if(arr1[a] === arr2[b]){
+                answer.push(arr2[b]);
+                arr2.splice(b, 1);
+                break;
             }
         }
     }
-    return 'No intersection';
+    return answer;
 };
